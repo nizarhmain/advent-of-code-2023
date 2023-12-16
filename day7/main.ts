1467c0586bc315ac36b13d0b06b3b2273a34b77e
Nizar Hmain <hmain.nizar@gmail.com>
2023-12-16 14:52:49 +0100

    day 7 part p1

--- day7/main.ts
+++ day7/main.ts
@@ -1,9 +1,140 @@
 import { readFromFile } from "../util/readFromFile";
 
 console.log("ioquweoiuqwe");
+function countOccurrences(str: string, char: string) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      count++;
+    }
+  }
+  return count;
+}
+
+const powerMap = new Map([
+  ["A", 14],
+  ["K", 13],
+  ["Q", 12],
+  ["J", 11],
+  ["T", 10],
+  ["9", 9],
+  ["8", 8],
+  ["7", 7],
+  ["6", 6],
+  ["5", 5],
+  ["4", 4],
+  ["3", 3],
+  ["2", 2],
+]);
+
+type Hand = {
+  letter: string;
+  count: number;
+  bid: number;
+  card: string;
+};
+
+function calculateRank(s: string): Hand[] {
+  // A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, or 2
+  // find if there are some things that are repeated
+  const card_bid = s.split(" ");
+  const card = card_bid[0];
+  const bid = card_bid[1];
+  const x = card.split("");
+
+  // for each char, see if there's more of it
+  // console.log(card);
+
+  const uniq = [...new Set(x)];
+  const str: Hand[] = uniq.map((letter) => {
+    return {
+      letter: letter,
+      count: countOccurrences(card, letter),
+      bid: Number(bid),
+      card: card,
+    };
+  });
+
+  return str;
+}
 
 const args: string[] = process.argv;
 const filePath: string = args[2];
 
 // solve for 1 star
-readFromFile(filePath);
+readFromFile(filePath).then((x) => {
+  const cards = x.map((card) => {
+    return calculateRank(card);
+  });
+
+  // before
+  // console.log(cards);
+
+  cards.sort((a: Hand[], b: Hand[]) => {
+    const maxA = Math.max(...a.map((x) => x.count));
+    const maxB = Math.max(...b.map((x) => x.count));
+
+    console.log("----");
+    console.log(a, b);
+    console.log("----");
+
+    if (maxA < maxB) {
+      return -1;
+    } else if (maxA > maxB) {
+      return 1;
+    }
+
+    // if they have the same max rank
+    // check if you have one pair, or two pair for example
+    const pairsForA = a.reduce((acc, curr) => {
+      if (curr.count === 2) {
+        acc = acc + 1;
+      }
+      return acc;
+    }, 0);
+
+    const pairsForB = b.reduce((acc, curr) => {
+      if (curr.count === 2) {
+        acc = acc + 1;
+      }
+      return acc;
+    }, 0);
+
+    if (pairsForA > pairsForB) {
+      return 1;
+    } else if (pairsForB > pairsForA) {
+      return -1;
+    }
+
+    function checkNextCard(i: number): number {
+      if (i < 5) {
+        if (
+          (powerMap.get?.(a[0].card[i]) ?? 0) >
+          (powerMap.get?.(b[0].card[i]) ?? 0)
+        ) {
+          return 1;
+        } else if (
+          (powerMap.get?.(a[0].card[i]) ?? 0) <
+          (powerMap.get?.(b[0].card[i]) ?? 0)
+        ) {
+          return -1;
+        } else {
+          // they are the same, call the function again on the [1] instead of [0]
+          return checkNextCard(i + 1);
+        }
+      } else {
+        return 0;
+      }
+    }
+    // return the highest first card
+    return checkNextCard(0);
+  });
+
+  const w = cards.reduce((acc, curr, i) => {
+    console.log(`rank: ${i + 1}: ${curr[0].bid * (i + 1)}`);
+    console.log(curr);
+    acc = acc + curr[0].bid * (i + 1);
+    return acc;
+  }, 0);
+  console.log(w);
+});
--- util/readFromFile.ts
+++ util/readFromFile.ts
@@ -1,8 +1,7 @@
 import * as fs from "fs";
-import { convert, findRaces, findWays } from "./main";
 
-export function readFromFile(filePath: string): Promise<number> {
-  const x = new Promise<number>((resolve, reject) => {
+export function readFromFile(filePath: string): Promise<string[]> {
+  const x = new Promise<string[]>((resolve, reject) => {
     fs.readFile(filePath, "utf8", (err, data: string) => {
       if (err) {
         console.error("cannot read");
@@ -12,15 +11,8 @@ export function readFromFile(filePath: string): Promise<number> {
 
         // solving for part two
         // 7 15 30 means one race with 71530 instead, combine all the numbers
-        const vert_l = convert(splitString[0]).length;
-        const races = findRaces(vert_l, splitString);
-
-        const result = findWays(races);
-
-        console.log(result);
-
         // for each race, figure out how many ways there are to solve them
-        resolve(1);
+        resolve(splitString);
       }
     });
   });
